f2fe207c2e6f70469f22bb28e80dbce85fdac675
Brett Seifried <seifriedbrett@gmail.com>
2022-01-24 13:47:08 -0800

    passing header test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,21 @@
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import App from './App'
 
-test('Should render the header', () => {
+test('Should render the header', async () => {
+  render(<App />)
 
+  //   const profile = screen.getByRole('paragraph', {
+  //     name: /meet/i,
+  //   })
+
+  const profile = screen.getByText(/meet/i)
+  expect(profile).toBeInTheDocument()
+
+  const logo = await screen.findByAltText(/alchemy logo/i)
+  expect(logo).toBeInTheDocument()
+
+  const name = await screen.findByText(/vonta/i)
+  expect(name).toBeInTheDocument()
+
+  //   await waitForElementToBeRemoved(() => screen.getByTestId(/loading/i))
 })
